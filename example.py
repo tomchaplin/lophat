d52de4c06969c2c5163d39ca92e085291ee4556a
Thomas Chaplin <tomrchaplin@gmail.com>
2023-03-20 14:28:29 +0000

    Update example

--- example.py
+++ example.py
@@ -1,7 +1,4 @@
-from lophat import (
-    compute_pairings,
-    LoPhatOptions,
-)
+from lophat import compute_pairings, LoPhatOptions
 
 # Note that I don't tell lophat what dimension my columns are
 matrix = [
@@ -21,29 +18,32 @@ matrix = [
     [4, 5, 6],
 ]
 
-# Note we pass iter(matrix)
-dgm_default = compute_pairings(iter(matrix))
+# Can pass in matrix either as List[List[int]] or Iterator[List[int]]
+
+dgm_iter = compute_pairings(matrix)
+dgm_list = compute_pairings(iter(matrix))
+
+# Can optionally provide a LoPhatOptions
 # Don't maintain V, use 4 threads, assume matrix is square,
 # ensure each thread gets at least 2 colums at a time
 opts = LoPhatOptions(False, 4, None, 2)
-dgm_custom = compute_pairings(iter(matrix), opts)
+dgm_custom = compute_pairings(matrix, opts)
 
-dgm_no_iter = compute_pairings(matrix)
 
-print("Default:")
-print(dgm_default.unpaired)
-print(dgm_default.paired)
+print("Iterator:")
+print(dgm_iter.unpaired)
+print(dgm_iter.paired)
+
+print("List:")
+print(dgm_list.unpaired)
+print(dgm_list.paired)
 
 print("Custom:")
 print(dgm_custom.unpaired)
 print(dgm_custom.paired)
 
-print("No iter:")
-print(dgm_no_iter.unpaired)
-print(dgm_no_iter.paired)
+assert dgm_iter.unpaired == dgm_custom.unpaired
+assert dgm_iter.paired == dgm_custom.paired
 
-assert dgm_default.unpaired == dgm_custom.unpaired
-assert dgm_default.paired == dgm_custom.paired
-
-assert dgm_default.unpaired == dgm_no_iter.unpaired
-assert dgm_default.paired == dgm_no_iter.paired
+assert dgm_iter.unpaired == dgm_list.unpaired
+assert dgm_iter.paired == dgm_list.paired
